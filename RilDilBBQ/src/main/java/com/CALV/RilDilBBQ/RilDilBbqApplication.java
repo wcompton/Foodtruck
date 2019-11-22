957f6c8817d0f80b35a0748d3569166012c46d58
Bill <53192167+wcompton@users.noreply.github.com>
2019-11-22 14:41:03 -0700

    Add files via upload

---
+++ RilDilBBQ/src/main/java/com/CALV/RilDilBBQ/RilDilBbqApplication.java
@@ -0,0 +1,13 @@
+package com.CALV.RilDilBBQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RilDilBbqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RilDilBbqApplication.class, args);
+	}
+
+}
